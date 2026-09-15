3cf253fc2499fac9ebbc27a16023cbfcc44abb4d
kparker1567 <kparker956@boyslatin.net>
2026-09-15 15:11:06 +0000

    Operator Scenario Activity

---
+++ Unit 1/operatorsActivity.py
@@ -0,0 +1,19 @@
+#Scenario 1.) Arithmetic Operator because its calculating a balance
+#Scenario 2.) Logical Operator because both conditions need to be true
+#Scenario 3.) This would also be Logical Operator because if one of the emergency contact numbers are true, then the school would call
+ #that one.
+ #Scenario 4.) Comparison Operator because its comparing how much you spent, before giving you a discount.
+ #Scenario 5.) Arithmetic Operator
+
+
+
+
+
+
+
+
+
+ print(200 == 100) # false
+ print(13 > 18) # false
+ print ("Coding 1" == "coding1") # false
+ print(0 != 0) # false
