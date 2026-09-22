1439fb96c8cb6008c87933b757cd8c73c0e4c97c
kparker1567 <kparker956@boyslatin.net>
2026-09-22 15:08:34 +0000

    class notes

--- Unit 1/operatorsActivity.py
+++ Unit 1/operatorsActivity.py
@@ -17,3 +17,32 @@
  print(13 > 18) # false
  print ("Coding 1" == "coding1") # false
  print(0 != 0) # false
+
+ #Scenario 1
+ book = 10.99
+tablet = 399.99
+print(book + tablet)
+
+#Scenario 2
+cart = 98.87
+amounttoGetDiscount = 100.00
+print(cart != amounttoGetDiscount)
+
+#Scenario 3
+shoes = 200.00
+Tax = 0.07
+print(200.00 * 0.07)
+
+#Scenario 4
+GPA = 86
+Recommendation = True
+
+print(GPA > 85 and Recommendation == True)
+
+
+parentContact1 = False
+parentContact2 = True
+
+print(parentContact1 == True or parentContact2 == True)
+
+
---
+++ Unit 2/builtinFunctions.py
@@ -0,0 +1,48 @@
+# Function - simply put; a code block of
+# instructions for computers to follow.
+
+# Built- in Function- a code block of
+# instructions for a computer to follow
+# that was already written for us.
+# PRE-WRITTEN CODE INSTRUCTIONS
+
+# print ()
+# value3 = input('what school do you go to')
+# print(value3)
+
+
+
+
+
+# Data casting functions
+# these are built in functions (pre-written instructions)
+# that change data types from one form into another
+
+# str() - this datacasting function allows you to change
+# any data type into a string
+
+year = 1906
+print('this event took place in '+ str(year))
+
+# string concatenation - combining string data types
+# with one another. We use the plus sign (+) operator
+# to combine strings together
+print( 3 + 3)  # adding 2 integer
+print("7" + "4") # COMBINING 2 strings
+print( "good" + "bye") 
+
+# int()- Any data type passed into the brackets will
+# be converted into a integer(whole number)
+
+num1 = input("type in a number") 
+#input always returns a string
+print( 4 + num1)
+
+
+
+
+
+
+
+
+# float()-
